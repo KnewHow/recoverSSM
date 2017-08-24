9d7a7746f52308af8f2d4f03d319fcbf6bfe1c37
yghtest <948170910@qq.com>
2017-08-24 21:33:37 +0800

    commit

---
+++ mybatis_spring_test/src/erongdu/dao/UserDaoImpl.java
@@ -0,0 +1,14 @@
+package erongdu.dao;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+
+import erongdu.pojo.User;
+
+public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao {
+
+	@Override
+	public User getUserById(int id) {
+			return this.getSqlSession().selectOne("test.findUserById", id);
+	}
+
+}
